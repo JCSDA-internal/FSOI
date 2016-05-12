13f7463d953281e5732f99cfafedb4f5fe2f6173
Rahul Mahajan <rahul.mahajan@noaa.gov>
2016-05-12 20:51:07 +0000

    get_platforms.py is now get_metadata.py and lists all platforms, obtypes on the platform and channels for radiance observations
    git-svn-id: https://svnemc.ncep.noaa.gov/projects/sandbox/Rahul.Mahajan/trunk/projects/FSOI@75474 281fb692-3a18-0410-826d-9e847983f365

--- scripts/get_metadata.py
+++ scripts/get_metadata.py
@@ -9,7 +9,7 @@
  ###############################################################
 
 '''
-get_platforms.py - List platforms and observation types
+get_metadata.py - List metadata such as platform, observation types and channels
 '''
 
 import sys
@@ -17,26 +17,26 @@ from argparse import ArgumentParser,ArgumentDefaultsHelpFormatter
 
 import lib_utils as lutils
 
-parser = ArgumentParser(description = 'Get platforms and observation types',formatter_class=ArgumentDefaultsHelpFormatter)
+parser = ArgumentParser(description = 'Get metadata from a center',formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument('-c','--center',help='originating center',type=str,required=True,choices=['EMC','GMAO','NRL','JMA_adj','JMA_ens','MET'])
 parser.add_argument('-r','--rootdir',help='root path to directory',type=str,default='/scratch3/NCEPDEV/stmp2/Rahul.Mahajan/test/Thomas.Auligne/FSOI',required=False)
-parser.add_argument('-o','--obtypes',help='show observation type for each platform',action='store_true',required=False)
 args = parser.parse_args()
 
 rootdir = args.rootdir
 center = args.center
-show_obtypes = args.obtypes
 
 fname = '%s/work/%s/bulk_stats.h5' % (rootdir,center)
 df = lutils.readHDF(fname,'df')
 
 platforms = df.index.get_level_values('PLATFORM').unique()
 for platform in sorted(platforms):
-    print '   %s' % platform
-    if show_obtypes:
-        tmp = df.xs(platform,level='PLATFORM',drop_level=False)
-        obtypes = tmp.index.get_level_values('OBTYPE').unique()
-        for obtype in obtypes:
-            print '     %s' % obtype
+    print '%s' % platform
+    tmp = df.xs(platform,level='PLATFORM',drop_level=False)
+    obtypes = tmp.index.get_level_values('OBTYPE').unique()
+    print '   | ' + ' '.join(obtypes)
+    if 'Tb' in obtypes:
+        channels = tmp.index.get_level_values('CHANNEL').unique()
+        print '   | ' + ' '.join(map(str,sorted(channels)))
+    print
 
 sys.exit(0)
