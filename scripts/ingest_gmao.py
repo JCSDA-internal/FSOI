b833661fe1f4a0ddb4f39fe45f0e05753cb659ce
David Hahn <hahnd@ucar.edu>
2019-03-14 15:12:53 -0600

    Updated the IOSingest CloudFormation template to include resources to download and monitor GMAO data. Updated the ingest_gmao.py script to download one file per thread.  Downloading files serially was taking > 15 minutes, and reaching the runtime limit in AWS Lambda.  Now takes < 3 minutes.

--- scripts/ingest_gmao.py
+++ scripts/ingest_gmao.py
@@ -2,7 +2,75 @@ import os
 import time
 import datetime
 import urllib3
+import certifi
 import re
+import boto3
+import json
+from threading import Thread
+
+# Get a shared S3 client to be used by all threads
+s3_client = boto3.client('s3')
+
+
+class Worker(Thread):
+    """
+    Thread to download file and put data in S3
+    """
+    def __init__(self, url, s3_bucket, s3_key):
+        """
+        Constructor
+        :param url: Source URL
+        :param s3_bucket: Target S3 bucket
+        :param s3_key: Target S3 key
+        """
+        super(Worker, self).__init__()
+        self.url = url
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.download_size = 0
+        self.success = False
+
+    def run(self):
+        """
+        Download a file from the website
+        :return: None
+        """
+        try:
+
+            # read the data from the URL
+            https = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+            response = https.request('GET', self.url)
+
+            # check that the response is OK (200)
+            if response.status != 200:
+                print('Website responded with %d' % response.status)
+                print('URL: %s' % self.url)
+                print(response.data.decode())
+                return
+
+            # write the data to S3
+            s3_response = s3_client.put_object(
+                Bucket=self.s3_bucket,
+                Key=self.s3_key,
+                Body=response.data
+            )
+
+            # check for an error in the response
+            if s3_response['ResponseMetadata']['HTTPStatusCode'] != 200:
+                print('Error uploading data to S3')
+                print('URL: %s' % self.url)
+                print('S3 URL: s3://%s/%s' % (self.s3_bucket, self.s3_key))
+                return
+            else:
+                print('Done: %s' % self.s3_key)
+
+            # happy
+            self.download_size = len(response.data)
+            self.success = True
+
+        except Exception as e:
+            print(e)
+            self.success = False
 
 
 def get_list_of_files_from_url(url):
@@ -12,7 +80,7 @@ def get_list_of_files_from_url(url):
     :return: {list} A list of files at this URL (file name only), or None if an error occurred
     """
     # read the data on the website with a GET request
-    https = urllib3.PoolManager()
+    https = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
     response = https.request('GET', url)
     if response.status != 200:
         print('Website responded with %d' % response.status)
@@ -22,40 +90,8 @@ def get_list_of_files_from_url(url):
 
     # parse the file names from the data
     data = response.data.decode()
-    files = re.findall('f522.*?\.ods', data, re.MULTILINE)
+    files = re.findall('GEOS.*?.ods', data, re.MULTILINE)
     return list(set(files))
-
-
-def download_file(url, output_file):
-    """
-    Download a file from the website
-    :param url: {str} The file url
-    :param output_file: The output file
-    :return: {bool} True if file was downloaded
-    """
-    # ensure the local path exists
-    local_path = output_file[:output_file.rfind('/')]
-    if not os.path.exists(local_path):
-        os.makedirs(local_path)
-
-    # read the data from the URL
-    https = urllib3.PoolManager()
-    response = https.request('GET', url)
-
-    # check that the response is OK (200)
-    if not response.status == 200:
-        print('Website responded with %d' % response.status)
-        print('URL: %s' % url)
-        print(response.data.decode())
-        return False
-
-    # write the data to a file
-    file = open(output_file, 'wb')
-    file.write(response.data)
-    file.close()
-
-    # happy
-    return True
 
 
 def main(event, context):
@@ -65,33 +101,73 @@ def main(event, context):
     :param context: {NoneType} Not used, but required for Lambda
     :return: None
     """
-    # TODO: Delete this block once the environment is setup
-    os.environ['LAG_IN_DAYS'] = '8'
-    os.environ['HTTPS_HOST'] = 'portal.nccs.nasa.gov'
-    os.environ['REMOTE_PATH'] = 'datashare/gmao_ops/pub/f522_fp/.internal/obs/Y%04d/M%02d/D%02d/H%02d/'
-
     log = {'ok': False, 'runtime': int(time.time()), 'size': -1, 'file_count': 0, 'name': 'n/a'}
 
-    lag = int(os.environ['LAG_IN_DAYS'])
-    https_host = os.environ['HTTPS_HOST']
-    remote_path = os.environ['REMOTE_PATH']
-    cycle_hour = 0
+    if event is None and context is None:
+        print('No event or context')
 
-    # compute the base url with the date
-    date = datetime.datetime.utcfromtimestamp(time.time() - lag * 86400)
-    remote_path = remote_path % (date.year, date.month, date.day, cycle_hour)
-    base_url = 'https://%s/%s' % (https_host, remote_path)
-    files = get_list_of_files_from_url(base_url)
+    try:
 
-    # download the data files
-    file_count = 0
-    for remote_file in files:
-        output_file = '/tmp/ods/Y%04d/M%02d/D%02d/H%02d/%s' % \
-                      (date.year, date.month, date.day, cycle_hour, remote_file)
-        file_count += 1 if download_file('%s/%s' % (base_url, remote_file), output_file) else 0
+        # get environment variables
+        lag = int(os.environ['LAG_IN_DAYS'])
+        https_host = os.environ['HTTPS_HOST']
+        remote_path = os.environ['REMOTE_PATH']
+        bucket = os.environ['BUCKET_NAME']
+        cycle_hour = 0
 
-    print('Downloaded %d file.' % file_count)
+        # compute the base url with the date
+        date = datetime.datetime.utcfromtimestamp(time.time() - lag * 86400)
+        remote_path = remote_path % (date.year, date.month, date.day, cycle_hour)
+        base_url = 'https://%s/%s' % (https_host, remote_path)
+        files = get_list_of_files_from_url(base_url)
+
+        # move the data files to S3
+        threads = []
+        s3_key_template = 'Y%04d/M%02d/D%02d/H%02d/%s'
+        for remote_file in files:
+            print('Transferring %s' % remote_file)
+            key = s3_key_template % (date.year, date.month, date.day, cycle_hour, remote_file)
+            url = '%s/%s' % (base_url, remote_file)
+            thread = Worker(url, bucket, key)
+            thread.start()
+            threads.append(thread)
+
+        # wait for all threads to finish and collect results
+        for thread in threads:
+            thread.join()
+            if thread.success:
+                log['file_count'] += 1
+                log['size'] += thread.download_size
+                log['ok'] = True
+
+        # TODO: submit a Batch job to process the new data
+        # submit_batch_job(s3_bucket, s3_key_template, files)
+
+        # print the log info for CloudWatch
+        print(json.dumps(log))
+
+    except Exception as e:
+        print(e)
+        print(json.dumps(log))
 
 
 if __name__ == '__main__':
+    from argparse import ArgumentParser
+    from argparse import ArgumentDefaultsHelpFormatter as HelpFormatter
+
+    DEFAULT_HOST = 'portal.nccs.nasa.gov'
+    DEFAULT_PATH = '/datashare/gmao_ops/pub/f522_fp/.internal/obs/Y%04d/M%02d/D%02d/H%02d'
+    DEFAULT_BUCKET = 'fsoi-gmao-ingest'
+    parser = ArgumentParser(description='Download GMAO data', formatter_class=HelpFormatter)
+    parser.add_argument('--lag', help='download data from N days ago', type=str, required=True)
+    parser.add_argument('--host', help='portal.nccs.nasa.gov', type=str, default=DEFAULT_HOST)
+    parser.add_argument('--remote-path', help='Remote path template', default=DEFAULT_PATH)
+    parser.add_argument('--bucket-name', help='S3 bucket name', default=DEFAULT_BUCKET)
+    args = parser.parse_args()
+
+    os.environ['LAG_IN_DAYS'] = args.lag
+    os.environ['HTTPS_HOST'] = args.host
+    os.environ['REMOTE_PATH'] = args.remote_path
+    os.environ['BUCKET_NAME'] = args.bucket_name
+
     main(None, None)
