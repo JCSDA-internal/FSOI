7ccdd703df56050fe2ad59229427b4701d228dc7
David Hahn <hahnd@ucar.edu>
2019-05-02 13:44:25 -0600

    Moved the 'process_stats' script into the fsoi.stats module.

--- python/setup.py
+++ python/setup.py
@@ -33,7 +33,7 @@ setup(
       'process_gmao=fsoi.ingest.gmao.process_gmao:main',
       'ingest_gmao=fsoi.ingest.gmao.__init__:download_and_process_gmao',
 
-      'process_stats=fsoi.ingest.process_stats:main',
+      'process_stats=fsoi.stats.process_stats:main',
       'batch_wrapper=fsoi.web.batch_wrapper:main'
     ]
   }
