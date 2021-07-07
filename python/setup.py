e531e2c2c6de4cebda3290ffcd91ad3528a462fe
Fabio L R Diniz <fabiolrdiniz@gmail.com>
2021-07-07 17:26:30 -0300

    replacing requires by install_requires

--- python/setup.py
+++ python/setup.py
@@ -12,9 +12,9 @@ setup(
   packages=['fsoi', 'fsoi.data', 'fsoi.ingest', 'fsoi.ingest.emc', 'fsoi.ingest.gmao', 'fsoi.ingest.jma',
             'fsoi.ingest.met', 'fsoi.ingest.meteofr', 'fsoi.ingest.nrl', 'fsoi.plots', 'fsoi.stats',
             'fsoi.web', 'fsoi.ingest.merra'],
-  requires=['bokeh==1.4.0', 'pyyaml', 'boto3', 'botocore', 'certifi',
-            'matplotlib', 'numpy', 'pandas', 'requests',
-            'urllib3', 'tables', 'fortranformat', 'netCDF4', 'phantomjs', 'selenium'],
+  install_requires=['bokeh==1.4.0', 'pyyaml', 'boto3', 'botocore', 'certifi',
+                    'matplotlib', 'numpy', 'pandas', 'requests',
+                    'urllib3', 'tables', 'fortranformat', 'netCDF4', 'phantomjs', 'selenium'],
   package_dir={'fsoi': 'src/fsoi'},
   package_data={
     'fsoi': [
